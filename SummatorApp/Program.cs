6083be9279a30d26b1ec370d9a45c8d3f1ece65f
silviya-iv <104994959+silviya-iv@users.noreply.github.com>
2022-06-03 22:15:24 +0300

    Add files via upload

---
+++ SummatorApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Summator
+{
+    internal class Program
+    {
+        static void Main (string[]args)
+        {
+             Console.WriteLine(Summator.Sum(new int[]{1000000000, 1500000000, 2000000000}));
+           
+
+        }
+    }
+} 
